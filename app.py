c21f2b91234b42a3c98372ebb669ae8ec772da92
Eric Youngson <eric@successionecological.com>
2016-12-20 20:53:02 -0800

    Added new credentials for Facebook 'test' app, added Heroku files

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ app.config['SECRET_KEY'] = 'top secret!'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['OAUTH_CREDENTIALS'] = {
     'facebook': {
-        'id': '470154729788964',
-        'secret': '010cc08bd4f51e34f3f3e684fbdea8a7'
+        'id': '238392349927343',
+        'secret': 'fb23159c117080db3c46fffddf0c409f'
     },
     'twitter': {
         'id': '3RzWQclolxWZIMq5LJqzRZPTl',
